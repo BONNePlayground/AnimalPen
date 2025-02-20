583aad82b8ebd192a96541b335245b30ecfe87cb
BONNe <bonne@bonne.id.lv>
2025-02-20 22:28:26 +0200

    Update to 1.21.4

--- common/src/main/java/lv/id/bonne/animalpen/client/AnimalPenClient.java
+++ common/src/main/java/lv/id/bonne/animalpen/client/AnimalPenClient.java
@@ -10,15 +10,11 @@ package lv.id.bonne.animalpen.client;
 import dev.architectury.registry.client.rendering.BlockEntityRendererRegistry;
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import dev.architectury.registry.client.rendering.RenderTypeRegistry;
-import dev.architectury.registry.item.ItemPropertiesRegistry;
-import lv.id.bonne.animalpen.AnimalPen;
 import lv.id.bonne.animalpen.blocks.renderer.AnimalPenRenderer;
 import lv.id.bonne.animalpen.blocks.renderer.AquariumRenderer;
 import lv.id.bonne.animalpen.registries.AnimalPenBlockRegistry;
 import lv.id.bonne.animalpen.registries.AnimalPenTileEntityRegistry;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.component.DataComponents;
-import net.minecraft.resources.ResourceLocation;
 
 
 public class AnimalPenClient
@@ -30,10 +26,6 @@ public class AnimalPenClient
         BlockEntityRendererRegistry.register(AnimalPenTileEntityRegistry.AQUARIUM_TILE_ENTITY.get(),
             context -> new AquariumRenderer());
         RenderTypeRegistry.register(RenderType.translucent(), AnimalPenBlockRegistry.AQUARIUM.get());
-
-        ItemPropertiesRegistry.registerGeneric(ResourceLocation.fromNamespaceAndPath(AnimalPen.MOD_ID, "filled_cage"),
-            ((itemStack, clientLevel, livingEntity, i) ->
-                itemStack.has(DataComponents.ENTITY_DATA) ? 1.0f : 0.0f));
 
         ColorHandlerRegistry.registerBlockColors(new WaterTankColor(), AnimalPenBlockRegistry.AQUARIUM);
     }
