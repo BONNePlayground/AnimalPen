64e730a9978ca08a638919bd5b656b9e33209911
BONNe <bonne@bonne.id.lv>
2026-01-10 20:05:10 +0200

    Remove unused tags

--- common/src/main/java/lv/id/bonne/animalpen/data/provider/ModItemTagsProvider.java
+++ common/src/main/java/lv/id/bonne/animalpen/data/provider/ModItemTagsProvider.java
@@ -26,22 +26,19 @@ public interface ModItemTagsProvider
         this.modTag(AnimalPenTags.ANIMAL_PEN_ATTACK_TOOLS).
             optionalTag(TagKey.create(Registries.ITEM, ResourceLocation.tryParse("swords")).location()).
             optionalTag(TagKey.create(Registries.ITEM, ResourceLocation.tryParse("axes")).location()).
-            optionalTag(ResourceLocation.tryParse("forge:knives")).
-            optionalTag(ResourceLocation.tryParse("forge:tools/knives")).
+            optionalTag(ResourceLocation.tryParse("c:tools/knife")).
             optionalTag(ResourceLocation.tryParse("c:knives"));
 
         this.modTag(AnimalPenTags.AQUARIUM_ATTACK_TOOLS).
             optionalTag(TagKey.create(Registries.ITEM, ResourceLocation.tryParse("swords")).location()).
             optionalTag(TagKey.create(Registries.ITEM, ResourceLocation.tryParse("axes")).location()).
-            optionalTag(ResourceLocation.tryParse("forge:knives")).
-            optionalTag(ResourceLocation.tryParse("forge:tools/knives")).
+            optionalTag(ResourceLocation.tryParse("c:tools/knife")).
             optionalTag(ResourceLocation.tryParse("c:knives"));
 
         this.modTag(AnimalPenTags.AVIARY_ATTACK_TOOLS).
             optionalTag(TagKey.create(Registries.ITEM, ResourceLocation.tryParse("swords")).location()).
             optionalTag(TagKey.create(Registries.ITEM, ResourceLocation.tryParse("axes")).location()).
-            optionalTag(ResourceLocation.tryParse("forge:knives")).
-            optionalTag(ResourceLocation.tryParse("forge:tools/knives")).
+            optionalTag(ResourceLocation.tryParse("c:tools/knife")).
             optionalTag(ResourceLocation.tryParse("c:knives"));
 
         this.modTag(AnimalPenTags.ANIMAL_PEN_ITEMS).copy(AnimalPenTags.ANIMAL_PEN_BLOCKS);
