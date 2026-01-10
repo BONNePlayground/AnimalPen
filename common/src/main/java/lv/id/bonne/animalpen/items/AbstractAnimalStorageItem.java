ea473ff2338c78e746c43642f5d7881059941a21
BONNe <bonne@bonne.id.lv>
2026-01-10 09:55:04 +0200

    Remove UUID from captured animal

--- common/src/main/java/lv/id/bonne/animalpen/items/AbstractAnimalStorageItem.java
+++ common/src/main/java/lv/id/bonne/animalpen/items/AbstractAnimalStorageItem.java
@@ -290,6 +290,10 @@ public abstract class AbstractAnimalStorageItem extends Item
         {
             CompoundTag animalTag = new CompoundTag();
             mob.save(animalTag);
+
+            animalTag.remove("UUID");
+            animalTag.remove("Pos");
+
             tag.put(AnimalPenCompoundTags.TAG_ANIMAL, animalTag);
         }
 
