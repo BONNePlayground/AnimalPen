a3cacdd41b7f6bde5b931085bb9b1633f46e983e
BONNe <bonne@bonne.id.lv>
2026-01-19 21:23:59 +0200

    Upgrade to minecraft 1.21.5

--- common/src/main/java/lv/id/bonne/animalpen/blocks/AviaryBlock.java
+++ common/src/main/java/lv/id/bonne/animalpen/blocks/AviaryBlock.java
@@ -5,7 +5,6 @@ import com.mojang.serialization.MapCodec;
 import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
-import lv.id.bonne.animalpen.blocks.entities.AbstractAnimalPenBlockEntity;
 import lv.id.bonne.animalpen.blocks.entities.AviaryTileEntity;
 import lv.id.bonne.animalpen.registries.AnimalPenTags;
 import lv.id.bonne.animalpen.registries.AnimalPenTileEntityRegistry;
@@ -13,7 +12,6 @@ import lv.id.bonne.animalpen.registries.AnimalPensItemRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -26,7 +24,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -92,46 +89,6 @@ public class AviaryBlock extends AbstractAnimalContainerBlock<AviaryTileEntity>
         }
 
         return use;
-    }
-
-
-    /**
-     * This method drops all items from container when block is broken.
-     *
-     * @param state The BlockState.
-     * @param level Level where block is broken.
-     * @param pos Position of broken block.
-     * @param newState New block state.
-     * @param isMoving Boolean if block is moving.
-     */
-    @Override
-    public void onRemove(BlockState state,
-        @NotNull Level level,
-        @NotNull BlockPos pos,
-        BlockState newState,
-        boolean isMoving)
-    {
-        if (!newState.is(AnimalPenTags.AVIARIES_BLOCKS))
-        {
-            BlockEntity tile = level.getBlockEntity(pos);
-
-            if (tile instanceof AbstractAnimalPenBlockEntity entity)
-            {
-                for (int i = 0; i < entity.getInventory().getContainerSize(); i++)
-                {
-                    Containers.dropItemStack(level,
-                        pos.getX(),
-                        pos.getY(),
-                        pos.getZ(),
-                        entity.getInventory().getItem(i));
-                }
-
-                entity.getInventory().clearContent();
-                level.updateNeighbourForOutputSignal(pos, this);
-            }
-
-            level.removeBlockEntity(pos);
-        }
     }
 
 
