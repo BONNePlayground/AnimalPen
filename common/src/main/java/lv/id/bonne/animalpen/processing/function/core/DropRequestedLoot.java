9561b438114bb7fe22d40b9f22566afb683b04b4
BONNe <bonne@bonne.id.lv>
2026-03-13 20:25:28 +0200

    Upgrade to 1.20

--- common/src/main/java/lv/id/bonne/animalpen/processing/function/core/DropRequestedLoot.java
+++ common/src/main/java/lv/id/bonne/animalpen/processing/function/core/DropRequestedLoot.java
@@ -18,7 +18,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
@@ -46,8 +46,9 @@ public class DropRequestedLoot implements EntityFunction.ProcessEntityFunction
 
         ResourceLocation lootTableKey = ResourceLocation.tryParse(dataValue.getAsString());
 
-        LootTable lootTable = serverLevel.getServer().getLootTables().get(lootTableKey);
-        LootContext context = new LootContext.Builder(serverLevel).
+        LootTable lootTable = serverLevel.getServer().getLootData().getLootTable(lootTableKey);
+
+        LootParams context = new LootParams.Builder(serverLevel).
             withParameter(LootContextParams.THIS_ENTITY, mob).
             withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(blockPos)).
             create(LootContextParamSets.GIFT);
