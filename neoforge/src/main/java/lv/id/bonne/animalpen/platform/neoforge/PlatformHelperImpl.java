2bc25fb94a94354627b7a88d0d9a907675e8b9ad
BONNe <bonne@bonne.id.lv>
2026-03-13 20:47:22 +0200

    Upgrade to 1.20.6

--- neoforge/src/main/java/lv/id/bonne/animalpen/platform/neoforge/PlatformHelperImpl.java
+++ neoforge/src/main/java/lv/id/bonne/animalpen/platform/neoforge/PlatformHelperImpl.java
@@ -1,15 +1,14 @@
-package lv.id.bonne.animalpen.platform.forge;
+package lv.id.bonne.animalpen.platform.neoforge;
 
 
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.SpawnEggItem;
-import net.minecraftforge.common.ForgeSpawnEggItem;
 
 
 public class PlatformHelperImpl
 {
     public static SpawnEggItem getSpawnEgg(EntityType<?> entityType)
     {
-        return ForgeSpawnEggItem.fromEntityType(entityType);
+        return SpawnEggItem.byId(entityType);
     }
 }
