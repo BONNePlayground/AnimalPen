dad869743c7aa8905710e6b2909df25f5cd46fd9
BONNe <bonne@bonne.id.lv>
2026-05-07 16:03:00 +0300

    Implement missing block placement restrictions

--- common/src/main/java/lv/id/bonne/animalpen/mixin/MixinLevelChunk.java
+++ common/src/main/java/lv/id/bonne/animalpen/mixin/MixinLevelChunk.java
@@ -8,6 +8,8 @@ package lv.id.bonne.animalpen.mixin;
 
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import lv.id.bonne.animalpen.registries.AnimalPenTags;
 import net.minecraft.world.level.block.Block;
@@ -18,14 +20,13 @@ import net.minecraft.world.level.chunk.LevelChunk;
 @Mixin(LevelChunk.class)
 public class MixinLevelChunk
 {
-    // TODO: requires migration
-//    @Redirect(method = "setBlockState",
-//        at = @At(value = "INVOKE",
-//            target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z"))
-    private boolean injected(BlockState instance, Block block)
+    @Redirect(method = "setBlockState",
+        at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/world/level/block/state/BlockState;is(Ljava/lang/Object;)Z"))
+    private boolean injected(BlockState instance, Object block)
     {
-        return instance.is(block) ||
+        return instance.is((Block) block) ||
             instance.is(AnimalPenTags.AVIARIES_BLOCKS) &&
-                block.builtInRegistryHolder().is(AnimalPenTags.AVIARIES_BLOCKS);
+                ((Block) block).builtInRegistryHolder().is(AnimalPenTags.AVIARIES_BLOCKS);
     }
 }
